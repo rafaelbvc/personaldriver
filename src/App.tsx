5b4820b9a77f865ad3ab899e553748ad502a6321
RafaelVendramini <rafaelbvc@hotmail.com>
2026-03-19 10:53:09 -0300

    V 0.0.1 - Small change on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Meu amor Gih
-        </p>
-        <a
-          //className="App-link"
-          //href= ""
-          //target="_blank"
-          //rel="noopener noreferrer"
-        >
-          Que seja para sempre
-        </a>
+
       </header>
+      <></>
     </div>
   );
 }
