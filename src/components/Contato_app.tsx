0ec23bbdc4c871863989f2564c9182296a361d1f
RafaelVendramini <rafaelbvc@hotmail.com>
2026-03-25 16:06:31 -0300

    v-0.1.9 - Pages Basics

---
+++ src/components/Contato_app.tsx
@@ -0,0 +1,9 @@
+import "./Contato_app_css.css"
+
+export const Contato_app = () => {
+    return (
+        <div className="contato_app_css">
+            <p>Contato_app</p>
+        </div>
+    )
+}
---
+++ src/components/Midia_app.tsx
@@ -0,0 +1,11 @@
+import "./Midia_app_css.css"
+
+
+export const Midia_app = () => {
+    return (
+
+        <div className="midia_app_css">
+            <p>Midia_app</p>
+        </div>
+    )
+}
