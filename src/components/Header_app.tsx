912e2924fdb1eac11adccc8bdbd84e42ba1adc69
RafaelVendramini <rafaelbvc@hotmail.com>
2026-03-26 22:59:56 -0300

    v-0.2.2 - Css Basics

--- src/components/Header_app.tsx
+++ src/components/Header_app.tsx
@@ -6,11 +6,22 @@ export const Header_app = () => {
     return (
         <div className="header_app_css">
             <div className="menu_header_css">
-                <p>Inicio</p>
-                <p>Painel</p>
-                <p>Mídia</p>
-                <p>Contato</p>
+
+                <p className="inicio_menu_css">Inicio</p>
+                <p className="painel_menu_css">Painel</p>
+                <p className="midia_menu_css">Mídia</p>
+                <p className="contato_menu_css">Contato</p>
+
+
             </div>
+            <div className="login_header_css">
+
+                <p>Login</p>
+                <p>Register</p>
+
+            </div>
+
+
         </div>
     )
 }
