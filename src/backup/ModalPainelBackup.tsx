09c7eef6dbb61df2d16e9e303a68563c4c0dd583
RafaelVendramini <rafaelbvc@hotmail.com>
2026-04-08 19:43:34 -0300

    v-0.5.4 - Small Refactor

---
+++ src/backup/ModalPainelBackup.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+import "./Modal_css.css"
+import { TModal } from "../types/TModal"
+
+
+
+
+
+
+export default function ModalPainel({ isOpen, children, setModalOpen }: TModal) {
+
+    if (isOpen) {
+        return (
+            <div className="background_modal_css">
+                <div className="modal_modal_css">
+                    <nav>
+                        <Link to="/" onClick={setModalOpen} >
+                            <button style={{ fontSize: "14px", marginLeft: "432px" }}>
+                                X
+                            </button>
+                        </Link>
+                    </nav>
+                    <p className="content_modal_css" >Bem vindo Marco, qual seu login?</p>
+                    <div>{children}</div>
+
+                </div>
+            </div>
+
+        )
+    }
+
+    return null
+}
